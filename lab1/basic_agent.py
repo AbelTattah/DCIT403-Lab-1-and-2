10f9222c970298a1e7647503b8f5d0500e7460e0
Abel Tattah <122866505+AbelTattah@users.noreply.github.com>
2026-01-29 21:18:18 +0000

    fix: patch to fix syntax errors

--- lab1/basic_agent.py
+++ lab1/basic_agent.py
@@ -22,17 +22,17 @@ class BasicAgent(Agent):
         self.my_behav.counter = 0
         self.add_behaviour(self.my_behav)
 
-if __name__ == "__main__":
-    # Ensure to use a valid JID and password. 
-    # If using embedded server, localhost should work if mapped correctly or registered.
-    dummy = BasicAgent("basic_agent@localhost", "password")
-    future = dummy.start()
-    future.result() # Wait for agent to start
+async def main():
+    dummy = BasicAgent("abeltattah@chat.between-us.online", "123Konka@")
+    await dummy.start()
 
     print("Agent started. Press Ctrl+C to quit.")
     try:
-        while True:
-            time.sleep(1)
+        while dummy.is_alive():
+            await asyncio.sleep(1)
     except KeyboardInterrupt:
-        dummy.stop()
+        await dummy.stop()
         print("Agent stopped")
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- lab2/sensor_agent.py
+++ lab2/sensor_agent.py
@@ -31,15 +31,17 @@ class SensorAgent(Agent):
         b = self.SensingBehaviour(period=2) 
         self.add_behaviour(b)
 
-if __name__ == "__main__":
-    agent = SensorAgent("sensor_agent@localhost", "password")
-    future = agent.start()
-    future.result()
+async def main():
+    agent = SensorAgent("abeltattah@chat.between-us.online", "123Konka@")
+    await agent.start()
 
     print("SensorAgent started. Press Ctrl+C to quit.")
     try:
-        while True:
-            time.sleep(1)
+        while agent.is_alive():
+            await asyncio.sleep(1)
     except KeyboardInterrupt:
-        agent.stop()
+        await agent.stop()
         print("SensorAgent stopped")
+
+if __name__ == "__main__":
+    asyncio.run(main())
